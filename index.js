4d864b3500f0b78f941a74961c888aac3ad1398c
amnanadeem247 <159475427+amnanadeem247@users.noreply.github.com>
2024-03-20 01:02:51 +0500

    Work Completed

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const chalk_1 = __importDefault(require("chalk"));
+const blessed = "😇 ";
+const smilee = "😊";
+const byHeart = "💖";
+const Ramzan = "A Month Of Blessings";
+console.log(`${chalk_1.default.bgYellow.bold.italic.greenBright(Ramzan)} ${blessed}`);
+let wishYouAll = " Happy Ramazan To all Of You  My dear Friend and All Group Members";
+console.log(`${chalk_1.default.bgBlue.bold.italic.greenBright(wishYouAll)} ${smilee}${byHeart}`);
+let Blessings = " “May Allah is always there to bless in each and every endeavor of your life. Warm greetings on the occasion of Ramadan to you and your family.” ";
+console.log(`${chalk_1.default.bgGreen.bold.italic.greenBright(Blessings)}`);
---
+++ index.ts
@@ -0,0 +1,21 @@
+import chalk from "chalk";
+ const blessed: any = "😇 "
+ const smilee: any  = "😊"
+ const byHeart: any = "💖"
+
+ const Ramzan: string = "A Month Of Blessings";
+
+ console.log( `${chalk.bgYellow.bold.italic.greenBright
+    (Ramzan)} ${blessed}`)
+
+ let wishYouAll: string = " Happy Ramazan To all Of You  My dear Friend and All Group Members";
+
+ console.log( `${chalk.bgBlue.bold.italic.greenBright
+(wishYouAll)} ${smilee}${byHeart}`)
+
+ let Blessings: string=" “May Allah is always there to bless in each and every endeavor of your life. Warm greetings on the occasion of Ramadan to you and your family.” ";
+
+ console.log( `${chalk.bgGreen.bold.italic.greenBright
+    (Blessings)}`)
+
+
